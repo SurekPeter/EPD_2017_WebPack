d2b41ae977bda6bc6fb51b1125c36093ba206a4a
surek.peter@gmail.com <surek.peter@gmail.com>
2017-02-22 20:45:41 +0100

    Prepare index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,10 @@ let buttonParent = $("<a>");
 buttonParent.html('Click on me!')
 $("body").prepend(buttonParent);
 
-if (document.querySelectorAll('a').length) {
-    require.ensure(['./Components/Button'], () => {
-        const Button = require('./Components/Button').default;
-        document.querySelectorAll('a').forEach((each) => {
-            let button = new Button($(each));
-            button.render('Close me!!!');
-        });
-    });
-}
-
-if (document.querySelectorAll('h1').length) {
-    require.ensure(['./Components/Header'], () => {
-        const Header = require('./Components/Header');
-        new Header().render('h1');
-    });
-}
+document.querySelectorAll('a').forEach((each) => {
+    let button = new Button($(each));
+    button.render('Close me!!!');
+});
 
 
 
